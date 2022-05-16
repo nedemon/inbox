7f5ec77eb1f09b4d526860f8f7c8067c92c7f915
Sergey Tsvetkov <sergey.tsvetkov@invent.us>
2022-05-16 19:54:30 +0300

    initial commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
